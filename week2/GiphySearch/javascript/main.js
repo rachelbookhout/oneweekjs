ecc55cc5f3e8ad1f965f49d9fc3499b8b5d00850
rachelbookhout <rbookhou@gmail.com>
2016-11-15 23:05:42 -0500

    test

---
+++ week2/GiphySearch/javascript/main.js
@@ -0,0 +1,33 @@
+//grab the input
+
+document.querySelector(".js-go").addEventListener("click", function(){
+  var input = document.querySelector("input").value;
+  pushtoDOM(input);
+});
+
+
+document.querySelector(".js-userinput").addEventListener("keyup", function(){
+  var input = document.querySelector("input").value;
+  if(e.which == 13){
+    pushtoDOM(input);
+  }
+});
+//do the data stuff with teh api
+
+var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC" ;
+var GiphyAJAXCall = new XMLHttpRequest();
+GiphyAJAXCall.open('GET', url);
+GiphyAJAXCall.send();
+GiphyAJAXCall.addEventListener('load', function(e){
+  var data = e.target.response;
+  pushtoDOM(data);
+});
+
+//show me the gifs
+function pushtoDOM(input){
+ var response =  JSON.parse[input];
+ var imageUrl = response.data[0].images.fixed_height_url;
+var container = document.querySelector(".js-container");
+container.innerHTML = "<img src=" + imgUrl +"/>";
+
+}
