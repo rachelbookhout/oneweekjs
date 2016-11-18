c26d716f9013c7487e729a4338bbf75e6f14de4e
rachelbookhout <rbookhou@gmail.com>
2016-11-17 23:06:14 -0500

    can't get the photos up

--- week2/TvTime/javascript/main.js
+++ week2/TvTime/javascript/main.js
@@ -1,10 +1,6 @@
-//grab the input
-
 document.querySelector(".js-go").addEventListener("click", function(){
-  var node = document.getElementsByClassName("js-container")[0];
-  while (node.hasChildNodes()) {
-    node.removeChild(node.firstChild);
-  }
+  var container = document.querySelector(".js-container");
+  container.innerHTML = '';
   var input = document.querySelector("input").value;
   searchGiphy(input);
 });
@@ -12,24 +8,24 @@ document.querySelector(".js-go").addEventListener("click", function(){
 
 document.querySelector(".js-userinput").addEventListener("keyup", function(e){
   var input = document.querySelector("input").value;
+  var container = document.querySelector(".js-container");
   if(e.which === 13){
-    var node = document.getElementsByClassName("js-container")[0];
-    while (node.hasChildNodes()) {
-      node.removeChild(node.firstChild);
-    }
-  searchGiphy(input);
+    container.innerHTML = '';
+    searchGiphy(input);
   }
 });
 
-
 //add a function when hitting the stop button
+document.querySelector("#stop").addEventListener("click", function(){
+  document.getElementById('container').classList.remove('active');
+  document.querySelector(".js-container").innerHTML = '';
 
-
+});
 
 //do the data stuff with the api
 
  function searchGiphy(input){
-  console.log(input + "in search");
+  document.getElementById('container').classList.add('active');
   var url = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=dc6zaTOxFJmzC";
   var GiphyAJAXCall = new XMLHttpRequest();
   GiphyAJAXCall.open('GET', url);
@@ -40,15 +36,38 @@ document.querySelector(".js-userinput").addEventListener("keyup", function(e){
   });
  }
 
+//
+//function ChangePhoto(image){
+  //console.log(image);
+  //container.innerHTML = '';
+  //console.log("I cleared the inner");
+  //var src = image.images.fixed_height.url;
+  //container.innerHTML = "<img src=\"" + src + "\">"
+  //console.log("I added an image");
+//}
+
 
 //show me the gifs
 function pushtoDOM(input){
   var response =  JSON.parse(input);
   var container = document.querySelector(".js-container");
   var imageUrls = response.data;
-  imageUrls.forEach(function(image){
-    var src = image.images.fixed_height.url;
-    //instead of adding, play with them one after another
-    container.innerHTML += "<img src=\"" + src + "\">";
-  });
+  //while (document.getElementById("container").classList.contains('active')){
+  if (document.getElementById("container").classList.contains('active')){
+     imageUrls.forEach(function(image){
+        console.log(container.innerHTML);
+        container.innerHTML = '';
+        var src = image.images.fixed_height.url;
+        setTimeout(function(){
+          container.innerHTML = "<img src=\"" + src + "\">"
+        },100000);
+        console.log("I timed out");
+    });
+  }
+  //setTimeout(AddPhoto(image,10000));
+
+
+  //}
+
+
 }
