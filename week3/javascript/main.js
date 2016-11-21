d9cf806902d2a26753a2edb0388b41f2c3c37980
rachelbookhout <rbookhou@gmail.com>
2016-11-21 16:28:33 -0500

    more stuff

--- week3/javascript/main.js
+++ week3/javascript/main.js
@@ -2,8 +2,25 @@
 
 
 //query soundcloud api
+var SoundCloudAPI = {};
 
+SoundCloudAPI.init = function(){
+  SC.initialize({
+    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+  });
+}
 
+SoundCloudAPI.init();
+
+SoundCloudAPI.getTrack = function(inputValue){
+  SC.get('/tracks', {
+    q: inputValue
+    }).then(function(tracks) {
+    console.log(tracks);
+  });
+}
+
+SoundCloudAPI.getTrack("Rilo Kiley");
 //display the cards
 
 
