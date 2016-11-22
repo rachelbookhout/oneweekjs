f647659ab44309bf6e3aa40847d3f7252989ee04
rachelbookhout <rbookhou@gmail.com>
2016-11-21 20:46:15 -0500

    got images

--- week3/javascript/main.js
+++ week3/javascript/main.js
@@ -23,5 +23,44 @@ SoundCloudAPI.getTrack = function(inputValue){
 SoundCloudAPI.getTrack("Rilo Kiley");
 //display the cards
 
+SoundCloudAPI.renderTracks = function(){
+  var card = document.createElement('div');
+  card.classList.add('card');
 
+  var imageDiv = document.createElement('div');
+  imageDiv.classList.add('image');
+
+  var image_img = document.createElement('img');
+  image_img.classList.add('image_img');
+  image_img.src = "http://www.placekitten.com/290/290";
+
+  imageDiv.appendChild(image_img);
+
+   var content = document.createElement('div');
+    content.classList.add('content');
+
+   var header = document.createElement('div');
+    header.classList.add('header');
+    header.innerHTML = '<a href="https://soundcloud.com/barsuk-records/rilo-kiley-science-vs-romance" target="_blank">"Science Vs. Romance"</a>';
+
+    var button = document.createElement('div');
+    button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+    var icon = document.createElement('i');
+    icon.classList.add("icon", "add");
+
+    var buttonText = document.createElement('span');
+    buttonText.innerHTML = "Add to Playlist";
+
+   content.appendChild(header);
+    button.appendChild(icon);
+    button.appendChild(buttonText);
+    card.appendChild(imageDiv);
+    card.appendChild(content);
+    card.appendChild(button);
+  var searchResults = document.querySelector(".js-search-results");
+  searchResults.appendChild(card);
+}
+
+SoundCloudAPI.renderTracks();
 //add to playlist and play
